a87f29d40df1423f7b56cbb457de10caaeadf8fb
申瑞珉 (Ruimin Shen) <ruimin0shen@gmail.com>
2017-05-07 18:34:08 +0800

    reorg can customize its name

--- yolo2/function.py
+++ yolo2/function.py
@@ -19,10 +19,10 @@ import numpy as np
 import tensorflow as tf
 
 
-def reorg(net, stride=2):
+def reorg(net, stride=2, name='reorg'):
     batch_size, height, width, channels = net.get_shape().as_list()
     _height, _width, _channel = height // stride, width // stride, channels * stride * stride
-    with tf.name_scope('reorg') as name:
+    with tf.name_scope(name) as name:
         net = tf.reshape(net, [batch_size, _height, stride, _width, stride, channels])
         net = tf.transpose(net, [0, 1, 3, 2, 4, 5]) # batch_size, _height, _width, stride, stride, channels
         net = tf.reshape(net, [batch_size, _height, _width, -1], name=name)
