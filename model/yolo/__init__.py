9f3d9ecf0a19525fc3cabbd4845c3a419e4cf036
申瑞珉 (Ruimin Shen) <ruimin0shen@gmail.com>
2017-05-09 23:15:42 +0800

    bug fix

--- model/yolo/__init__.py
+++ model/yolo/__init__.py
@@ -70,30 +70,35 @@ class Model(object):
 class Objectives(dict):
     def __init__(self, model, mask, prob, coords, offset_xy_min, offset_xy_max, areas):
         self.model = model
-        self.mask = mask
-        self.prob = prob
-        self.coords = coords
-        self.offset_xy_min = offset_xy_min
-        self.offset_xy_max = offset_xy_max
-        self.areas = areas
-        with tf.name_scope('iou'):
+        with tf.name_scope('true'):
+            self.mask = tf.identity(mask, name='mask')
+            self.prob = tf.identity(prob, name='prob')
+            self.coords = tf.identity(coords, name='coords')
+            self.offset_xy_min = tf.identity(offset_xy_min, name='offset_xy_min')
+            self.offset_xy_max = tf.identity(offset_xy_max, name='offset_xy_max')
+            self.areas = tf.identity(areas, name='areas')
+        with tf.name_scope('iou') as name:
             _offset_xy_min = tf.maximum(model.offset_xy_min, self.offset_xy_min) 
             _offset_xy_max = tf.minimum(model.offset_xy_max, self.offset_xy_max)
             _wh = tf.maximum(_offset_xy_max - _offset_xy_min, 0.0)
             _areas = _wh[:, :, :, 0] * _wh[:, :, :, 1]
             areas = tf.maximum(self.areas + model.areas - _areas, 1e-10)
-            iou = tf.truediv(_areas, areas, name='iou')
+            iou = tf.truediv(_areas, areas, name=name)
         with tf.name_scope('mask'):
             max_iou = tf.reduce_max(iou, 2, True, name='max_iou')
             mask_max_iou = tf.to_float(tf.equal(iou, max_iou, name='mask_max_iou'))
             mask_best = tf.identity(self.mask * mask_max_iou, name='mask_best')
             mask_normal = tf.identity(1 - mask_best, name='mask_normal')
-        iou_diff = tf.identity(model.iou - iou, name='iou_diff')
+        with tf.name_scope('diff2'):
+            iou_diff2 = tf.pow(model.iou - mask_best, 2, name='iou_diff2')
+            coords_diff2 = tf.pow(model.coords - self.coords, 2, name='coords_diff2')
+            prob_diff2 = tf.pow(model.prob - self.prob, 2, name='prob_diff2')
         with tf.name_scope('objectives'):
-            self['iou_best'] = tf.nn.l2_loss(mask_best * iou_diff, name='iou_best')
-            self['iou_normal'] = tf.nn.l2_loss(mask_normal * iou_diff, name='iou_normal')
-            self['coords'] = tf.nn.l2_loss(tf.expand_dims(mask_best, -1) * (model.coords - self.coords), name='coords')
-            self['prob'] = tf.nn.l2_loss(tf.expand_dims(self.mask, -1) * model.prob - self.prob, name='prob')
+            self['iou_best'] = tf.reduce_sum(mask_best * iou_diff2, name='iou_best')
+            self['iou_normal'] = tf.reduce_sum(mask_normal * iou_diff2, name='iou_normal')
+            _mask_best = tf.expand_dims(mask_best, -1)
+            self['coords'] = tf.reduce_sum(_mask_best * coords_diff2, name='coords')
+            self['prob'] = tf.reduce_sum(_mask_best * prob_diff2, name='prob')
 
 
 class Builder(object):
@@ -119,7 +124,7 @@ class Builder(object):
                 self.hparam = dict([(key, tf.Variable(float(s), name='hparam_' + key, trainable=False)) for key, s in self.config.items(section + '_hparam')])
                 self.hparam_regularizer = tf.Variable(self.config.getfloat(section, 'hparam'), name='hparam_regularizer', trainable=False)
             with tf.name_scope('loss_objectives'):
-                loss_objectives = tf.reduce_sum([self.objectives[key] * self.hparam[key] for key in self.objectives], name='loss_objectives')
+                loss_objectives = tf.reduce_sum([tf.multiply(self.objectives[key], self.hparam[key], name='weighted_' + key) for key in self.objectives], name='loss_objectives')
             loss_regularizer = tf.identity(self.hparam_regularizer * self.model.regularizer, name='loss_regularizer')
             loss = tf.identity(loss_objectives + loss_regularizer, name=name)
         return loss
