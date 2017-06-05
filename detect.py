9d15ad8d03a71c470eb184e21c6124d697bbacd4
申瑞珉 (Ruimin Shen) <ruimin0shen@gmail.com>
2017-06-05 22:31:50 +0800

    auto rotate via exif; directory supporting

--- detect.py
+++ detect.py
@@ -20,13 +20,12 @@ import argparse
 import configparser
 import importlib
 import itertools
-import cv2
+from PIL import Image, ExifTags
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
-from tensorflow.python.framework import ops
 import utils.preprocess
 import utils.postprocess
 
@@ -39,57 +38,90 @@ def darknet(image):
     return image / 255.
 
 
+def read_image(path):
+    image = Image.open(path)
+    for key in ExifTags.TAGS.keys():
+        if ExifTags.TAGS[key] == 'Orientation':
+            break
+    exif = dict(image._getexif().items())
+    if exif[key] == 3:
+        image = image.rotate(180, expand=True)
+    elif exif[key] == 6:
+        image = image.rotate(270, expand=True)
+    elif exif[key] == 8:
+        image = image.rotate(90, expand=True)
+    return image
+
+
+def detect(sess, model, names, image, path):
+    preprocess = eval(args.preprocess)
+    _, height, width, _ = image.get_shape().as_list()
+    _image = read_image(path)
+    image_original = np.array(np.uint8(_image))
+    image_height, image_width, _ = image_original.shape
+    image_std = preprocess(np.array(np.uint8(_image.resize((width, height)))).astype(np.float32))
+    feed_dict = {image: np.expand_dims(image_std, 0)}
+    tensors = [model.conf, model.xy_min, model.xy_max]
+    conf, xy_min, xy_max = sess.run([tf.check_numerics(t, t.op.name) for t in tensors], feed_dict=feed_dict)
+    boxes = utils.postprocess.non_max_suppress(conf[0], xy_min[0], xy_max[0], args.threshold, args.threshold_iou)
+    scale = [image_width / model.cell_width, image_height / model.cell_height]
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.imshow(image_original)
+    colors = [prop['color'] for _, prop in zip(names, itertools.cycle(plt.rcParams['axes.prop_cycle']))]
+    cnt = 0
+    for _conf, _xy_min, _xy_max in boxes:
+        index = np.argmax(_conf)
+        if _conf[index] > args.threshold:
+            wh = _xy_max - _xy_min
+            _xy_min = _xy_min * scale
+            _wh = wh * scale
+            linewidth = min(_conf[index] * 10, 3)
+            ax.add_patch(patches.Rectangle(_xy_min, _wh[0], _wh[1], linewidth=linewidth, edgecolor=colors[index], facecolor='none'))
+            ax.annotate(names[index] + ' (%.1f%%)' % (_conf[index] * 100), _xy_min, color=colors[index])
+            cnt += 1
+    fig.canvas.set_window_title('%d objects detected' % cnt)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    return fig
+
+
 def main():
     model = config.get('config', 'model')
     yolo = importlib.import_module('model.' + model)
     width = config.getint(model, 'width')
     height = config.getint(model, 'height')
-    preprocess = eval(args.preprocess)
     with tf.Session() as sess:
-        image_bgr = cv2.imread(os.path.expanduser(os.path.expandvars(args.image)))
-        image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
-        image_std = preprocess(cv2.resize(image_rgb, (width, height))).astype(np.float32)
-        image_std = tf.expand_dims(ops.convert_to_tensor(image_std), 0)
+        image = tf.placeholder(tf.float32, [1, height, width, 3], name='image')
         builder = yolo.Builder(args, config)
-        builder(image_std)
+        builder(image)
         global_step = tf.contrib.framework.get_or_create_global_step()
         model_path = tf.train.latest_checkpoint(utils.get_logdir(config))
         tf.logging.info('load ' + model_path)
         slim.assign_from_checkpoint_fn(model_path, tf.global_variables())(sess)
         tf.logging.info('global_step=%d' % sess.run(global_step))
-        tensors = [builder.model.conf, builder.model.xy_min, builder.model.xy_max]
-        conf, xy_min, xy_max = sess.run([tf.check_numerics(t, t.op.name) for t in tensors])
-        boxes = utils.postprocess.non_max_suppress(conf[0], xy_min[0], xy_max[0], args.threshold, args.threshold_iou)
-        image_height, image_width, _ = image_rgb.shape
-        scale = [image_width / builder.model.cell_width, image_height / builder.model.cell_height]
-        fig = plt.figure()
-        ax = fig.gca()
-        ax.imshow(image_rgb)
-        colors = [prop['color'] for _, prop in zip(builder.names, itertools.cycle(plt.rcParams['axes.prop_cycle']))]
-        cnt = 0
-        for _conf, _xy_min, _xy_max in boxes:
-            index = np.argmax(_conf)
-            if _conf[index] > args.threshold:
-                wh = _xy_max - _xy_min
-                _xy_min = _xy_min * scale
-                _wh = wh * scale
-                linewidth = min(_conf[index] * 10, 3)
-                ax.add_patch(patches.Rectangle(_xy_min, _wh[0], _wh[1], linewidth=linewidth, edgecolor=colors[index], facecolor='none'))
-                ax.annotate(builder.names[index] + ' (%.1f%%)' % (_conf[index] * 100), _xy_min, color=colors[index])
-                cnt += 1
-        fig.canvas.set_window_title('%d objects detected' % cnt)
-        ax.set_xticks([])
-        ax.set_yticks([])
-        plt.show()
+        path = os.path.expanduser(os.path.expandvars(args.path))
+        if os.path.isfile(path):
+            detect(sess, builder.model, builder.names, image, path)
+            plt.show()
+        else:
+            for dirpath, _, filenames in os.walk(path):
+                for filename in filenames:
+                    if os.path.splitext(filename)[-1].lower() in args.exts:
+                        _path = os.path.join(dirpath, filename)
+                        print(_path)
+                        detect(sess, builder.model, builder.names, image, _path)
+                        plt.show()
 
 
 def make_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('image', help='input image')
+    parser.add_argument('path', help='input image path')
     parser.add_argument('-c', '--config', nargs='+', default=['config.ini'], help='config file')
     parser.add_argument('-p', '--preprocess', default='std', help='the preprocess function')
     parser.add_argument('-t', '--threshold', type=float, default=0.3)
     parser.add_argument('--threshold_iou', type=float, default=0.4, help='IoU threshold')
+    parser.add_argument('-e', '--exts', nargs='+', default=['.jpg', '.png'])
     parser.add_argument('--level', default='info', help='logging level')
     return parser.parse_args()
 
